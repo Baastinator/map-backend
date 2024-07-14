7a3bd77e695fd021e6afb0b34635504274a3ca0a
BuildTools <kring.basti.2004@gmail.com>
2024-07-14 04:43:04 +0200

    commit 4

--- src/log/log.service.spec.ts
+++ src/log/log.service.spec.ts
@@ -1,18 +1,22 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { LogService } from './log.service';
+import { mock } from 'jest-mock-extended';
+import { MysqlService } from '../mysql/mysql.service';
 
 describe('LogService', () => {
-  let service: LogService;
+  const setup = (): {
+    service: LogService;
+    mysqlService: MysqlService;
+  } => {
+    const mysqlService = mock<MysqlService>();
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [LogService],
-    }).compile();
+    const service = new LogService(mysqlService);
 
-    service = module.get<LogService>(LogService);
-  });
+    return { service, mysqlService };
+  };
 
   it('should be defined', () => {
+    const { service } = setup();
+
     expect(service).toBeDefined();
   });
 });
